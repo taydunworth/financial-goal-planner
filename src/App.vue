3f5d81752cae08d02698d2ffa2c55c3b458aa54d
Taylor Cox <taylorashtoncox@gmail.com>
2019-03-31 12:30:08 -0400

    Adds and styles content on sidebar

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,16 @@
           <span>Taylor Cox</span>
       </div>
     </div>
+
+    <div id="sidebar">
+      <span id="date">Friday, March 22, 2019</span>
+      <ul>
+        <li><i class="fas fa-plus"></i> Add Goal</li>
+        <li><i class="far fa-credit-card"></i> Integrate with Bank</li>
+        <li><i class="fas fa-chart-bar"></i> Track Progress</li>
+        <li><i class="fas fa-piggy-bank"></i>Savings Analysis</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -69,6 +79,47 @@ body {
 
 #user-info img {
   padding-right: 10px;
+}
+
+#sidebar {
+  background-color: #1F2838;
+  width: 22%;
+  height: calc(100vh - 70px);
+  color: #8f949c;
+  padding: 20px;
+}
+
+#sidebar ul {
+  list-style: none;
+  padding: 0;
+  margin-top: 50px;
+}
+
+#sidebar ul li {
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #414956;
+  transition: .3s color ease-in-out;
+}
+
+#sidebar ul li:last-of-type {
+  border-bottom: none;
+}
+
+#sidebar ul li:hover {
+  color: #fff;
+  cursor: pointer;
+}
+
+#sidebar .fas, #sidebar .far {
+  padding-right: 10px;
+}
+
+#date {
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 1px;
+  font-size: .8em;
 }
 
 </style>
